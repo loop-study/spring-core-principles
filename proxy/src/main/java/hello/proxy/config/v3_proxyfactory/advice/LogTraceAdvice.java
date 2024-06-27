e69d66ae4bafdc383ee0e62e72097889c4405f6a
apdkeb <apdkeb@naver.com>
2024-06-27 20:56:26 +0900

    섹션6. 스프링이 지원하는 프록시

---
+++ proxy/src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -0,0 +1,37 @@
+package hello.proxy.config.v3_proxyfactory.advice;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.lang.reflect.Method;
+
+public class LogTraceAdvice implements MethodInterceptor {
+
+    // 더 이상 타겟을 안 받음.
+    // 타겟이 누군지 신경안쓰고 오로지 부가기능만 구현하면 됨.
+    private final LogTrace logTrace;
+
+    public LogTraceAdvice(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        TraceStatus status = null;
+        try {
+            Method method = invocation.getMethod();
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = logTrace.begin(message);
+            // 로직 호출
+            Object result = invocation.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
